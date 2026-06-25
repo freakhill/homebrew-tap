b54bcbbca9e55bb13380e024a3c8048e109fc51a
freakhill <johan.gall@protonmail.com>
2026-06-26 04:40:12 +0900

    claudemetre 1.1.17

--- Casks/claudemetre.rb
+++ Casks/claudemetre.rb
@@ -1,6 +1,6 @@
 cask "claudemetre" do
-  version "1.1.16"
-  sha256 "2a6eacbefc8f73c31b808bae1a7d0f1969d9bae4f4b5f9159a002fc8c6c4c289"
+  version "1.1.17"
+  sha256 "1cc77b7d3e977bd4ec86cd95af099b6d5ee2f8471e0a510a30f6846719450697"
 
   url "https://github.com/freakhill/claudemetre/releases/download/v#{version}/claudemetre-#{version}.zip"
   name "claudemetre"
